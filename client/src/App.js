2d5940f343a0fac6701d6df8adc57b68a7fde743
RomanSchmid <rrr.schmid@gmail.com>
2022-09-13 11:44:40 +0200

    Add first heading, paragraph and image

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import cookbookLogo from "./cookbook-logo.svg"
+
+const cookbook = {
+  name: "Cookbook"
+};
+
+function displayHeading(heading) {
+  return <h1>My <span>{heading}</span></h1>
+}
 
 function App() {
   return (
     <div className="App">
-
+      {displayHeading(cookbook.name)}
+      <p>Vivamus suscipit tortor <span>eget felis porttitor</span> volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <img 
+        src={cookbookLogo}
+        alt="Logo" 
+        width="300"
+        height="300"
+        >
+      </img>
     </div>
   );
 }
